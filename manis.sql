b9f62e11638e9f0bbbe9ee9554a9140091e1dd16
EL <113089132+MSulaeman@users.noreply.github.com>
2025-02-04 08:33:19 +0700

    Add files via upload

---
+++ manis.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 04, 2025 at 02:32 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test_pra_ujikom`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `delete_procedure` (`id_kelas` INT(11))   BEGIN
+DELETE FROM tb_kelas WHERE id_kelas = id_kelas;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_procedure` (`id_kelas` INT(11), `nama` VARCHAR(30), `nama_walas` VARCHAR(30))   BEGIN
+INSERT INTO tb_kelas VALUES (id_kelas, nama, nama_walas);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `select_prod` ()   BEGIN
+SELECT * FROM tb_kelas;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_procedure` (`id_kelas` INT(11), `nama` VARCHAR(30), `nama_walas` VARCHAR(30))   BEGIN
+UPDATE tb_kelas SET nama = nama, nama_walas = nama_walas WHERE id_kelas = id_kelas;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logjur`
+--
+
+CREATE TABLE `logjur` (
+  `logId` int(11) NOT NULL,
+  `id_jurusan` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `logjur`
+--
+
+INSERT INTO `logjur` (`logId`, `id_jurusan`, `nama`) VALUES
+(1, 1, 'bugilang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jurusan`
+--
+
+CREATE TABLE `tb_jurusan` (
+  `id_jurusan` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_jurusan`
+--
+
+INSERT INTO `tb_jurusan` (`id_jurusan`, `nama`) VALUES
+(1, 'bugilang');
+
+--
+-- Triggers `tb_jurusan`
+--
+DELIMITER $$
+CREATE TRIGGER `inserttri` AFTER INSERT ON `tb_jurusan` FOR EACH ROW BEGIN
+INSERT INTO logjur SET id_jurusan=NEW.id_jurusan, nama=NEW.nama;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kelas`
+--
+
+CREATE TABLE `tb_kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `nama_walas` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_kelas`
+--
+
+INSERT INTO `tb_kelas` (`id_kelas`, `nama`, `nama_walas`) VALUES
+(12, 'rpl', 'oma'),
+(13, 'tkj', 'paguru'),
+(31, 'kelazz', 'buguru');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_siswa`
+--
+
+CREATE TABLE `tb_siswa` (
+  `nis` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `id_jurusan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `logjur`
+--
+ALTER TABLE `logjur`
+  ADD PRIMARY KEY (`logId`);
+
+--
+-- Indexes for table `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  ADD PRIMARY KEY (`id_jurusan`);
+
+--
+-- Indexes for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD PRIMARY KEY (`nis`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_jurusan` (`id_jurusan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `logjur`
+--
+ALTER TABLE `logjur`
+  MODIFY `logId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_jurusan`
+--
+ALTER TABLE `tb_jurusan`
+  MODIFY `id_jurusan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+
+--
+-- AUTO_INCREMENT for table `tb_kelas`
+--
+ALTER TABLE `tb_kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+
+--
+-- AUTO_INCREMENT for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  MODIFY `nis` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_siswa`
+--
+ALTER TABLE `tb_siswa`
+  ADD CONSTRAINT `tb_siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `tb_kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_siswa_ibfk_2` FOREIGN KEY (`id_jurusan`) REFERENCES `tb_jurusan` (`id_jurusan`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
